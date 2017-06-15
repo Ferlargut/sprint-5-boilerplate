4a13a2fa8d1252ae2abdec3da8b166d64f786f35
ferlargut <ferlargut@gmail.com>
2017-06-15 15:37:42 -0500

    Filtrado
    busca pero aun no tiene el mejor metodo para el filtrado

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,21 @@ function getParameterByName(name) {
     results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+// var filtrarContactos = function (e) {
+// 	e.preventDefault();
+// 	var criterioBusqueda = $("#search").val().toLowerCase();
+// 	var contactosFiltrados = contactos.filter(function (contacto) {
+// 		return contacto.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+// 	});
+// 	mostrarContactos(contactosFiltrados);
+// };
+// var mostrarContactos = function (contactos) {
+// 	var plantillaFinal = "";
+// 	contactos.forEach(function (contacto) {
+// 		plantillaFinal += plantillaContacto.replace("__nombre__", contacto.nombre)
+// 			.replace("__numero__", contacto.numero)
+// 			.replace("__foto__", contacto.foto);
+// 	});
+// 	$(".contacts").html(plantillaFinal);
+// };
--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
 //objeto donde se guardan las url que utilizare a lo largo del proyecto
 var url = {
-    url:" http://examen-laboratoria-sprint-5.herokuapp.com/topics",
+    url:" http://examen-laboratoria-sprint-5.herokuapp.com/topics",    
 };
 //lugar donde se mostraran las tareas
 var $topics =$("#topics");
 //funcion que al cargar ejecutara las demas funciones
 var cargarPagina = function () {
-    cargarTopics();
-    $("#add-form").submit(newTopic);    
+    cargarTopics();   
+    $("#add-form").submit(newTopic);  
+    $("#search-topic").click(filterTopic);  
 };
 //hago una peticion para obtener el json con el qeu trabajare
 //asi como ejecutar funcion de crear topicos y mostrar 
 var cargarTopics = function () {
     $.getJSON(url.url,function (topics) {
-        topics.forEach(crearTopics);
+    topics.forEach(crearTopics);
     });
 };
 // creo la plantilla que voy a usar para mostrarlo en el html 
 var plantillaTopic =    
         
-        "<article class='row' data-id='__id__'>"+
+        "<article class='row' data-url='__url__' data-id='__id__'>"+
             "<h3 class='col s 3'>'__nameauthor__'</h3>"+
             "<h4 class='col s 3'>'__content__'</h4>"+
         "</article>";        
@@ -30,16 +31,18 @@ var crearTopics = function (topic) {
     var content = topic.content;
     var id =topic.id;
     var topic ="";
-
-    topic = plantillaTopic.replace("__nameauthor__",name).replace("__content__",content).replace("__id__",id);
+    var ingresarUrl= url.url+"/"+id;        
+    
+    topic = plantillaTopic.replace("__nameauthor__",name).replace("__content__",content).replace("__id__",id).replace("__url__",ingresarUrl);
     $topics.append(topic);   
+    
 };
 //funcion para crear un nuevo topic en el JSON
 var newTopic = function (e) {
     e.preventDefault();
     var nameN = $("#name-author").val();
     var contentN =$("#content").val();
-        console.log(nameN,contentN,url.url);
+        
     $.post(url.url,
         {
           author_name: nameN,
@@ -47,8 +50,35 @@ var newTopic = function (e) {
         },
         function(topic){
             crearTopics(topic);            
-        });
-    
-}
+        });    
+};
 
+var filterTopic = function (){
+    $.getJSON(url.url,function (topics) {
+    var topicsSearch =  $("#search").val().toLowerCase();
+    var topicFS=[];           
+    var topicFilter = topics.filter(function (topic) {             
+        var fer = topic.author_name.toLowerCase()== topicsSearch; 
+        if(fer==true){
+            console.log (topic);
+            topicSearch(topic);
+        }
+    });   
+    topicFilter.forEach(topicSearch);
+   
+    });
+};
+var topicSearch = function (topic) {
+    var $search = $("#show-search");
+    console.log($search);
+    var name = topic.author_name;
+    var content = topic.content;
+    var id =topic.id;
+    var topic ="";
+    var ingresarUrl= url.url+"/"+id;        
+    
+    topic = plantillaTopic.replace("__nameauthor__",name).replace("__content__",content).replace("__id__",id).replace("__url__",ingresarUrl);
+    $search.append(topic);       
+}
+//modal
 $(document).ready(cargarPagina);
